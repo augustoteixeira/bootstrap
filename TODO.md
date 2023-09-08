1e3102099b0a75baf4cd6f58669533722de1de6c
Augusto Teixeira <augusto@impa.br>
2023-09-08 11:15:42 +0100

    Fix VLA bug

--- TODO.md
+++ TODO.md
@@ -1,9 +1,5 @@
-- investigate VLA bug
-- use alias for long double
-- compare performance with double
 - use double and store multiple of diagonal
 
 
 
-- check openmp in office
 - check halide for one line
--- src/modified.cpp
+++ src/modified.cpp
@@ -4,61 +4,66 @@
 
 //#include "omp.h"
 
-const long double LOG_MULTIPLE = 2.0;
-// defined below
-void c_modified(long double p, int s, void *, void *, void *, void *, int);
+typedef long double real;
+
+const real LOG_MULTIPLE = 1.5;
+// this updates a diagonal (s = a + b) with a given past. To be defined below.
+void c_modified(real p, int s, void*, void*, void*, void*, int);
 bool IsPowerOfTwo(ulong x) { return (x != 0) && ((x & (x - 1)) == 0); }
 // there exists an infection among k fixed points
-long double f(long double p, int k) { return 1 - powl(1 - p, k); }
+real f(real p, int k) { return 1 - powl(1 - p, k); }
 // there is no infection in these k points
-long double n(long double p, int k) { return powl(1 - p, k); }
+real n(real p, int k) { return powl(1 - p, k); }
 // normalizing function for image generation
-long double normalize(long double v) { return fmaxl(logl(v), -200); }
+real normalize(real v) { return fmaxl(logl(v), -200); }
 
 int main(int argc, char **argv) {
   {
     clock_t tic = clock(); // for timing purposes
-    int m_max = 6; // we let p run from 2^{-1}, 2^{-2}, ..., 2^{-m_max}.
+    int m_min = 4; // we let p run from 2^{-m_min}, 2^{-2}, ..., 2^{-m_max} (inclusive).
+    int m_max = 18;
     int m_for_image = 4; // this exponent will be used to build the table and the png image
-    if (m_max < m_for_image) { return 1; };
+    if (m_max < m_min) { return 1; };
+    //if (m_for_image < m_min) { return 1; };
+    //if (m_for_image > m_max) { return 1; };
 
-    // on my machine, long doubles are 16 bytes long, which is twice as much as double
+    // on my machine, reals are 16 bytes long, which is twice as much as double
 
     // image inicialization
-    long double p_for_image = powl(2.0, -m_for_image);
+    real p_for_image = powl(2.0, -m_for_image);
     int a_max_for_image = (int) LOG_MULTIPLE * logl(1 / p_for_image) / p_for_image;
-    long double (*image)[a_max_for_image] = (long double (*)[a_max_for_image])
-      malloc(sizeof(long double[a_max_for_image][a_max_for_image]));
-    for (int i = 0; i < a_max_for_image; i++) {
-      for (int j = 0; j < a_max_for_image; j++) {
-        image[i][j] = 0;
+    real (*image)[a_max_for_image] = (real (*)[a_max_for_image])
+      malloc(sizeof(real[a_max_for_image][a_max_for_image]));
+    for (int a = 0; a < a_max_for_image; a++) {
+      for (int b = 0; b < a_max_for_image; b++) {
+        image[a][b] = 0;
       }
     }
 
     // table dimensions
     int tab_h = 5;
     int tab_w = 6;
-    long double (*table)[tab_h][tab_w] = (long double (*)[tab_h][tab_w])
-      malloc(sizeof(long double[7][tab_h][tab_w]));
+    real (*table)[tab_h][tab_w] = (real (*)[tab_h][tab_w])
+      malloc(sizeof(real[7][tab_h][tab_w]));
 
     // loop through several values of p
-    for (int m = 1; m <= m_max; m++) {
-      long double p = powl(2, -m);
+    for (int m = m_min; m <= m_max; m++) {
+      real p = powl(2, -m);
       int a_max = (int) LOG_MULTIPLE * logl(1 / p) / p;
 
       // N0, N1, N2, N3 will rotate to progress on the calculation
       // one will be the current one, being updated and the others will be the
       // three past layers.
       // Note that we use 7 * a_max, one for each of the 7 states
-      long double (*N0)[a_max] = (long double (*)[a_max]) malloc(sizeof(long double[7][a_max]));
-      long double (*N1)[a_max] = (long double (*)[a_max]) malloc(sizeof(long double[7][a_max]));
-      long double (*N2)[a_max] = (long double (*)[a_max]) malloc(sizeof(long double[7][a_max]));
-      long double (*N3)[a_max] = (long double (*)[a_max]) malloc(sizeof(long double[7][a_max]));
+      real (*N0)[a_max] = (real (*)[a_max]) malloc(sizeof(real[7][a_max]));
+      real (*N1)[a_max] = (real (*)[a_max]) malloc(sizeof(real[7][a_max]));
+      real (*N2)[a_max] = (real (*)[a_max]) malloc(sizeof(real[7][a_max]));
+      real (*N3)[a_max] = (real (*)[a_max]) malloc(sizeof(real[7][a_max]));
       // these pointers will be set accordingly. To fill M(i,j), we use N(i+j mod 4)
-      long double (*current)[a_max];
-      long double (*past1)[a_max];
-      long double (*past2)[a_max];
-      long double (*past3)[a_max];
+      real (*current)[a_max];
+      real (*past1)[a_max];
+      real (*past2)[a_max];
+      real (*past3)[a_max];
       // initialize everything with zeros first
       for (int k = 0; k < 7 ; k++) {
         for (int s = 0; s < a_max; s++) {
@@ -91,6 +96,7 @@ int main(int argc, char **argv) {
           current = N3; past1 = N2; past2 = N1; past3 = N0;
           break;
         }
+
         // fill the current array with an auxiliary function (implemented below)
         c_modified(p, s, current, past1, past2, past3, a_max);
 
@@ -98,52 +104,43 @@ int main(int argc, char **argv) {
         if (m == m_for_image) {
           // fill table
           for (int k = 0; k < 7; k++) {
-            for (int i = 0; i < tab_w; i++) {
-              if (s - i <= tab_h) {
-                table[k][i][s - i] = current[k][i];
+            for (int a = 0; a < tab_w; a++) {
+              if (s - a <= tab_h) {
+                table[k][a][s - a] = current[k][a];
               }
             }
           }
           // fill image
-          for (int j = 1; j < s; j++) {
-            image[s-j][j] = current[0][j];
+          for (int a = 1; a < s; a++) {
+            image[s-a][a] = current[0][a];
           }
         }
         // if we are at the last diagonal, calculate max and sum and print
         if (s == a_max - 1) {
-          long double sum = 0;
-          long double max = 0;
+          real sum = 0;
+          real max = 0;
           for (int l = 0; l < a_max; l++) {
             if (max < current[0][l]) { max = current[0][l]; }
             sum += current[0][l];
           }
-          printf("%Lf, %Lf\n", -logl(p), -p * logl(max));
+          printf("%Lf, %Lf # m = %d\n", -logl(p), -p * logl(sum), m);
           //printf("m = %d, N = %d, logl(sum{M(x, N-x)}) = %Lf, -logl(middle) = %Lf\n",
           //       m, a_max, logl(sum), -logl(current[s/2]));
           fflush(stdout);
         }
       }
-      printf("A");          fflush(stdout);
-      free(N0);
-      printf("B");          fflush(stdout);
-      free(N1);
-      printf("C");          fflush(stdout);
-      free(N2);
-      printf("D");          fflush(stdout);
-      free(N3);
-      printf("E");          fflush(stdout);
     }
-    fprintf(stderr, "Elapsed in c_30: %Lf secs\n\n", (long double)(clock() - tic)/CLOCKS_PER_SEC);
+    fprintf(stderr, "Elapsed in c_30: %Lf secs\n\n", (real)(clock() - tic)/CLOCKS_PER_SEC);
 
     // print tables
     printf("table dimensions = %d by %d\n", tab_h, tab_w);
     for (int k = 0; k < 7; k++) {
       printf("table %d:\n", k);
-      for (int i = tab_w - 1; i > 0; i--) {
-        for (int j = 1; j < tab_h; j++) {
-          printf("M[%d, %d] = ", j, i);
-          long double value =
-            normalize(table[k][j][i]);
+      for (int b = tab_w - 1; b >= 0; b--) {
+        for (int a = 0; a < tab_h; a++) {
+          printf("M[%d, %d] = ", a, b);
+          real value =
+            normalize(table[k][a][b]);
           if (value == -200) {
             printf("         ");
           } else {
@@ -161,27 +158,27 @@ int main(int argc, char **argv) {
     printf("a_max_for_image = %d\n", a_max_for_image);
     printf("image size = %d\n", a_max_for_image * a_max_for_image);
     // normalize image
-    for (int i = 0; i < a_max_for_image; i++) {
-      for (int j = 0; j < a_max_for_image; j++) {
-        image[i][j] = normalize(image[i][j]);
+    for (int a = 0; a < a_max_for_image; a++) {
+      for (int b = 0; b < a_max_for_image; b++) {
+        image[a][b] = normalize(image[a][b]);
       }
     }
     // scale it to stay between zero and 255
-    long double max_image = -10000000000;
-    long double min_image = 1000000000;
-    for (int i = 0; i < a_max_for_image/2; i++) {
-      for (int j = 0; j < a_max_for_image/2; j++) {
-        if (image[i][j] > max_image) {
-          max_image = image[i][j];
+    real max_image = -10000000000;
+    real min_image = 1000000000;
+    for (int a = 0; a < a_max_for_image/2; a++) {
+      for (int b = 0; b < a_max_for_image/2; b++) {
+        if (image[a][b] > max_image) {
+          max_image = image[a][b];
         }
-        if (image[i][j] < min_image) {
-          min_image = image[i][j];
+        if (image[a][b] < min_image) {
+          min_image = image[a][b];
         }
       }
     }
-    for (int i = 0; i < a_max_for_image/2; i++) {
-      for (int j = 0; j < a_max_for_image/2; j++) {
-        image[i][j] = 255 * ((image[i][j] - min_image) / (max_image - min_image));
+    for (int a = 0; a < a_max_for_image/2; a++) {
+      for (int b = 0; b < a_max_for_image/2; b++) {
+        image[a][b] = 255 * ((image[a][b] - min_image) / (max_image - min_image));
       }
     }
     // write to file
@@ -208,7 +205,7 @@ int main(int argc, char **argv) {
 }
 
 void c_modified(
-  long double p,
+  real p,
   int s,
   void * o,
   void * p1,
@@ -216,10 +213,10 @@ void c_modified(
   void * p3,
   int a_max)
 {
-  long double (*output)[a_max] = static_cast<long double (*)[a_max]>(o);
-  long double (*past1)[a_max] = static_cast<long double (*)[a_max]>(p1);
-  long double (*past2)[a_max] = static_cast<long double (*)[a_max]>(p2);
-  long double (*past3)[a_max] = static_cast<long double (*)[a_max]>(p2);
+  real (*output)[a_max] = static_cast<real (*)[a_max]>(o);
+  real (*past1)[a_max] = static_cast<real (*)[a_max]>(p1);
+  real (*past2)[a_max] = static_cast<real (*)[a_max]>(p2);
+  real (*past3)[a_max] = static_cast<real (*)[a_max]>(p3);
   // pragma omp parallel for
   for (int a = 1; a < s; a++) {
     int b = s - a;
@@ -228,9 +225,11 @@ void c_modified(
     output[1][a] = (1 - p) * f(p, a) * past1[1][a]
       + n(p, b) * output[0][a] + p * past2[2][a - 1];
     output[5][a] = (1 - p) * f(p, a) * past1[5][a] + p * past2[4][a - 1];
-    // be careful!!! Here we are overflowing on purpose for a - 2,
-    // but this seems fine for testing
-    output[6][a] = (1 - p) * n(p, b) * past1[6][a - 1] + p * p * past3[3][a - 2];
+    if (a == 1) {
+        output[6][a] = (1 - p) * n(p, b) * past1[6][a - 1];
+    } else {
+      output[6][a] = (1 - p) * n(p, b) * past1[6][a - 1] + p * p * past3[3][a - 2];
+    }
     output[2][a] = (1 - p) * f(p, b) * past1[2][a - 1]
       + p * (1 - p) * past2[3][a - 1]
       + (1 - p) * n(p, a) * output[1][a] + (1 - p) * n(p, b) * output[6][a];
